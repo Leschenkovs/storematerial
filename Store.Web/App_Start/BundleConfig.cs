aada3b480a16ab16e072d26f116cb4c4ec3d3004
Vadzim Leschenko <leschenko@scand.com>
2016-04-15 18:42:02 +0300

    Added scripts to Web project

--- Store.Web/App_Start/BundleConfig.cs
+++ Store.Web/App_Start/BundleConfig.cs
@@ -7,23 +7,26 @@ namespace Store.Web.App_Start
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/Store.Web/angular").Include(
-											"~/packages/angularjs.1.5.3/content/Scripts/angular.js",
-											"~/packages/angularjs.1.5.3/content/Scripts/angular-sanitize.js",
-											"~/packages/Angular.UI.Bootstrap.1.3.1/content/Scripts/angular-ui/ui-bootstrap.js",
-											"~/packages/Angular.UI.Bootstrap.1.3.1/content/Scripts/angular-ui/ui-bootstrap-tpls.js",
-											"~/packages/Angular.UI.UI-Router.0.2.18/content/scripts/angular-ui-router.js",
-											"~/packages/angular-ui-date.1.0.0/content/Scripts/date.js"
-									));
+																			"~/Scripts/angular/angularjs.1.5.3/content/Scripts/angular.js",
+																			"~/Scripts/angular/angularjs.1.5.3/content/Scripts/angular-sanitize.js",
+																			"~/Scripts/angular/Angular.UI.Bootstrap.1.3.1/content/Scripts/angular-ui/ui-bootstrap.js",
+																			"~/Scripts/angular/Angular.UI.Bootstrap.1.3.1/content/Scripts/angular-ui/ui-bootstrap-tpls.js",
+																			"~/Scripts/angular/Angular.UI.UI-Router.0.2.18/content/scripts/angular-ui-router.js",
+																			"~/Scripts/angular/angular-ui-date.1.0.0/content/Scripts/date.js"
+															));
 
-			/*bundles.Add(new ScriptBundle("~/Store.Web/app").Include(
-											"~/Angular/app.js",
-											"~/Angular/app.config.js",
-											"~/Angular/helpers/*.js",
-											"~/Angular/services/*.js",
-											"~/Angular/directives/*.js",
-											"~/Angular/controllers/*.js"
-									));
-			*/
+			bundles.Add(new StyleBundle("~/Store.Web/css").Include(
+								"~/Content/styles/*.css"
+						));
+
+			bundles.Add(new ScriptBundle("~/Store.Web/app").Include(
+																			"~/Angular/app.js",
+																			"~/Angular/app.config.js",
+																			"~/Angular/helpers/*.js",
+																			"~/Angular/services/*.js",
+																			"~/Angular/directives/*.js",
+																			"~/Angular/controllers/*.js"
+															));
 
 			// Set EnableOptimizations to false for debugging. For more information,
 			// visit http://go.microsoft.com/fwlink/?LinkId=301862
