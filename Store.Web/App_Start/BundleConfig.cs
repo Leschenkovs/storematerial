fe1bc70a9f431ed7902254a0c6c30b91d3964e99
Nastya <novikova@home.com>
2016-04-17 01:39:58 +0300

    update

--- Store.Web/App_Start/BundleConfig.cs
+++ Store.Web/App_Start/BundleConfig.cs
@@ -2,38 +2,46 @@
 
 namespace Store.Web.App_Start
 {
-    public class BundleConfig
-    {
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            //bundles.Add(new ScriptBundle("~/ecportal/base").Include(
-            //			 ));
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+		  bundles.Add(new ScriptBundle("~/Store.Web/base").Include(
+			 "~/Scripts/jquery-1.9.1.min.js",
+			 "~/Scripts/bootstrap.js",
+			 "~/Scripts/bootstrap.min.js"
 
-            bundles.Add(new ScriptBundle("~/Store.Web/angular").Include(
-                "~/Scripts/angular/angular.js",
-                "~/Scripts/angular/angular-sanitize.js",
-                "~/Scripts/angular/ui-bootstrap.js",
-                "~/Scripts/angular/ui-bootstrap-tpls.js",
-                "~/Scripts/angular/angular-ui-router.js",
-                "~/Scripts/angular/date.js"
-                ));
+						));
 
-            bundles.Add(new StyleBundle("~/Store.Web/css").Include(
-                "~/Content/styles/*.css"
-                ));
+			bundles.Add(new ScriptBundle("~/Store.Web/angular").Include(
+				"~/Scripts/angular/common.js",
+				"~/Scripts/angular/modernizr.custom.js",
+				"~/Scripts/angular/angular.js",
+				"~/Scripts/angular/angular-sanitize.js",
+				"~/Scripts/angular/ui-bootstrap.js",
+				"~/Scripts/angular/ui-bootstrap-tpls.js",
+				"~/Scripts/angular/angular-ui-router.js",
+				"~/Scripts/angular/date.js"
+				));
 
-            bundles.Add(new ScriptBundle("~/Store.Web/app").Include(
-                "~/Angular/app.js",
-                "~/Angular/app.config.js",
-                "~/Angular/helpers/*.js",
-                "~/Angular/services/*.js",
-                "~/Angular/directives/*.js",
-                "~/Angular/controllers/*.js"
-                ));
+			bundles.Add(new StyleBundle("~/Store.Web/css").Include(
+			  "~/Content/styles/store.app.css",
+				"~/Content/styles/business-casual.css",
+				"~/Content/*.css"
+				));
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = false;
-        }
-    }
+			bundles.Add(new ScriptBundle("~/Store.Web/app").Include(
+				"~/Angular/app.js",
+				"~/Angular/app.config.js",
+				"~/Angular/helpers/*.js",
+				"~/Angular/services/*.js",
+				"~/Angular/directives/*.js",
+				"~/Angular/controllers/*.js"
+				));
+
+			// Set EnableOptimizations to false for debugging. For more information,
+			// visit http://go.microsoft.com/fwlink/?LinkId=301862
+			BundleTable.EnableOptimizations = false;
+		}
+	}
 }
