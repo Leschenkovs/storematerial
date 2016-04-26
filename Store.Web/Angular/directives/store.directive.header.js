c28ae3fc1cdc805317f75eeaaaea70d4c69fd167
Vadzim Leschenko <leschenko@scand.com>
2016-04-27 01:21:20 +0300

    Improved of main menu (part 2)

--- Store.Web/Angular/directives/store.directive.header.js
+++ Store.Web/Angular/directives/store.directive.header.js
@@ -10,7 +10,8 @@
             controller: function ($scope, $rootScope, $state) {
                 var off = $rootScope.$on('$stateChangeSuccess', function () {
                     var user = $rootScope.globals.currentUser;
-                    $scope.forUser = (user.role == 'admin' ? { width: '16.66666666666667%' } : { width: '20%' });
+                    if (user)
+                        $scope.forUser = (user.role == 'admin' ? { width: '16.66666666666667%' } : { width: '20%' });
                 });
                 $scope.$on('$destroy', off);
             }
